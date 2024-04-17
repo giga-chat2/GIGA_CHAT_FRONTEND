522f4ad68cd0798d437b997443a87a8ddc7814e7
Aditya Sakpal <112710558+Aditya-Sakpal@users.noreply.github.com>
2024-04-18 01:34:40 +0530

    Update auth.ts

--- apps/frontend/src/app/api/auth/[...nextauth]/auth.ts
+++ apps/frontend/src/app/api/auth/[...nextauth]/auth.ts
@@ -1,6 +1,7 @@
 import GoogleProvider from "next-auth/providers/google";
 import GithubProvider from "next-auth/providers/github";
 import TwitterProvider from "next-auth/providers/twitter";
+import FacebookProvider from "next-auth/providers/facebook";
 
 export const authOptions={
   providers : [
@@ -13,10 +14,14 @@ export const authOptions={
       clientSecret:process.env.NEXT_GITHUB_SECRET as string
     }),
     TwitterProvider({
-      clientId:process.env.NEXT_TWITTER_ID as string,
-      clientSecret:process.env.NEXT_TWITTER_SECRET as string,
+      clientId:'ODBmODJnbmNQbTVlUk1JM0dfQ2E6MTpjaQ',
+      clientSecret:'fwy93lHFybjubOi1koHF3pmWzoQ8MbVPLUb0CrT0xULF-tg1bd',
       version:"2.0"
     }),
+    FacebookProvider({
+      clientId:'805763111466139',
+      clientSecret:'2f98a79e7202c96c1050b664ab10875b'
+    })
     
   ]
 }
